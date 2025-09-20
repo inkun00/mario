a790e18ffc4ca5cb73eb600fcd0ff52c912bf2a6
Inkeun Yoo <inkun00@gmail.com>
2025-09-20 01:33:32 +0000

    한 번 로그인을 하면 같은 기기에서는 로그인 유지하게 해

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -16,6 +16,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
+setPersistence(auth, browserLocalPersistence);
+
 const db = getFirestore(app);
 
 export { app, auth, db };
