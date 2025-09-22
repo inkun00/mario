a7d469dc46c3be93252a2a3add2e76a0953170ac
Inkeun Yoo <inkun00@gmail.com>
2025-09-22 12:41:07 +0000

    이 컨테이너 가로 너비를 20% 늘려줘

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -6,7 +6,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-1 p-4 sm:p-6 md:p-8">
+      <main className="flex-1 p-4 sm:p-6 md:p-8 w-full max-w-7xl mx-auto">
         {children}
       </main>
     </div>
