df31e82ba568c4c7ee2434ac26f061d01e0b71f7
Inkeun Yoo <inkun00@gmail.com>
2025-09-23 01:36:17 +0000

    자꾸 에러가 발생해 개발 환경에서 에러가 없는데 빌드 환경에서 에러가 있다면 기능은 동일하되, 사용되는 언어나 패키지를 빌드 환경

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,9 +1,9 @@
-
 'use server';
 
 import { config } from 'dotenv';
 config();
 
+import '@/ai/flows/join-game-flow.ts';
 import '@/ai/flows/adjust-difficulty-flow.ts';
 import '@/ai/flows/check-nickname-flow.ts';
 import '@/ai/flows/analyze-learning-flow.ts';
@@ -11,6 +11,5 @@ import '@/ai/flows/generate-review-question-flow.ts';
 import '@/ai/flows/check-review-answer-flow.ts';
 import '@/ai_flows/update-scores-flow.ts';
 import '@/ai/flows/validate-quiz-set-flow.ts';
-import '@/ai/flows/join-game-flow.ts';
 
     
