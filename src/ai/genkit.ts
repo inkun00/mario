b16c40946ddf04c04d4976f924a9474610fa3f35
Inkeun Yoo <inkun00@gmail.com>
2025-09-23 11:39:29 +0000

    genkit 관련 내용도 있던데

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -14,5 +14,5 @@ import { googleAI } from '@genkit-ai/googleai';
 export const ai = genkit({
   plugins: [googleAI()],
   logLevel: 'debug',
-  enableTracingAndMetrics: true,
+  enableTracingAndMetrics: false,
 });
