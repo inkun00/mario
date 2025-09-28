2a4db64d1b9af3da13fc627c7fc8ce25671ff958
Inkeun Yoo <inkun00@gmail.com>
2025-09-28 06:20:33 +0000

    복습문제 만들기 버튼 에러 발생했어 이번에는 원인이 뭐야?

--- src/next.config.ts
+++ src/next.config.ts
@@ -38,9 +38,6 @@ const nextConfig: NextConfig = {
   },
   serverActions: {
     allowedOrigins: ["localhost:9002", "*.firebase.app", "*.web.app"]
-  },
-  env: {
-    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
   }
 };
 
