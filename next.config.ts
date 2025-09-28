a9131decaffdba2d764b31f7951b98b9cd333c3a
Inkeun Yoo <inkun00@gmail.com>
2025-09-28 10:46:44 +0000

    Runtime Error
    Error: GEMINI_API_KEY environment variable is not set. P

--- next.config.ts
+++ next.config.ts
@@ -37,8 +37,14 @@ const nextConfig: NextConfig = {
     return config;
   },
   serverActions: {
-    allowedOrigins: ["localhost:9002", "*.firebase.app", "*.web.app"]
-  }
+    allowedOrigins: ["localhost:9002", "*.firebase.app", "*.web.app"],
+    bodySizeLimit: '4.5mb',
+  },
+  experimental: {
+    httpAgentOptions: {
+      keepAlive: true,
+    },
+  },
 };
 
 export default nextConfig;
