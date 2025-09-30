aee05d3efd60e466f563979b914cf5f3e5969dd4
Inkeun Yoo <inkun00@gmail.com>
2025-09-30 12:52:57 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,33 +1,25 @@
-import { initializeApp, getApps, App, cert } from 'firebase-admin/app';
+import { initializeApp, getApps, App } from 'firebase-admin/app';
 import { getFirestore, Firestore } from 'firebase-admin/firestore';
-
-// Service account credentials can be stored in environment variables
-// For local development, you might use a serviceAccount.json file
-// but it's more secure to use environment variables in production.
-// const serviceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS 
-//   ? JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS) 
-//   : undefined;
 
 let adminDb: Firestore;
 
 function initializeAdmin() {
-  // Check if there are any initialized apps
+  const BATCH_REQUESTS_MAX = 500;
   if (getApps().length > 0) {
-    // If an app is already initialized, get the Firestore instance from it
     adminDb = getFirestore(getApps()[0]);
-  } else {
-    // If no app is initialized, initialize a new one
-    // const credential = serviceAccount ? cert(serviceAccount) : undefined;
-    initializeApp({
-      // credential
+    adminDb.settings({
+        batchRequests: BATCH_REQUESTS_MAX,
     });
+  } else {
+    initializeApp();
     adminDb = getFirestore();
+    adminDb.settings({
+        batchRequests: BATCH_REQUESTS_MAX,
+    });
   }
   return adminDb;
 }
 
-// Initialize and export the db instance.
-// This function ensures that initialization happens only once.
 adminDb = initializeAdmin();
 
 export { adminDb };
