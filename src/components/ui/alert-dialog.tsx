23d2bfabca9d1d1e48b0e445c823fc7098c6b78b
Inkeun Yoo <inkun00@gmail.com>
2025-12-30 17:18:28 +0000

    x버튼을 누르면 창이 닫히게 해

--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -43,10 +44,12 @@ const AlertDialogContent = React.forwardRef<
       {...props}
     >
       {props.children}
-       <AlertDialogCancel className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground h-auto w-auto p-1">
-        <X className="h-4 w-4" />
-        <span className="sr-only">Close</span>
-      </AlertDialogCancel>
+      <AlertDialogPrimitive.Cancel asChild>
+        <button className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+          <X className="h-4 w-4" />
+          <span className="sr-only">Close</span>
+        </button>
+      </AlertDialogPrimitive.Cancel>
     </AlertDialogPrimitive.Content>
   </AlertDialogPortal>
 ))
