58998973718169ffc72efae934a4963b9195ebc5
Inkeun Yoo <inkun00@gmail.com>
2026-01-10 09:54:56 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Hydration failed

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -11,9 +11,12 @@ export function useIsMobile() {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     }
     mql.addEventListener("change", onChange)
+    
+    // Set initial value after mount
     setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    
     return () => mql.removeEventListener("change", onChange)
   }, [])
 
-  return !!isMobile
+  return isMobile
 }
